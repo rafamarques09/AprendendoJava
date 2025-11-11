bfc2b4af8c1f54ace3cd40df868c53df33e8f8bd
rafael <04042811094@senacrs.edu.br>
2025-11-10 22:10:00 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/devdojo/apendendo/java/javacore/introducaometodos/dominio/Calculadora.java

--- src/devdojo/apendendo/java/javacore/introducaometodos/dominio/Calculadora.java
+++ src/devdojo/apendendo/java/javacore/introducaometodos/dominio/Calculadora.java
@@ -7,4 +7,20 @@ public class Calculadora {
     public void subtraiDoisNumeros(){
         System.out.println(10-4);
     }
+    public void multiplicaDoisNumeros(int num1, int num2){
+        System.out.println(num1*num2);
+    }
+    public double divideDoisNumeros(double num1, double num2){
+        if(num2 == 0){
+            return 0;
+        }
+        return num1/num2;
+    }
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("num1 "+num1);
+        System.out.println("num2 "+num2);
+    }
 }
---
+++ src/devdojo/apendendo/java/javacore/introducaometodos/test/EstudanteTest04.java
@@ -0,0 +1,15 @@
+package devdojo.apendendo.java.javacore.introducaometodos.test;
+
+import devdojo.apendendo.java.javacore.introducaometodos.dominio.Calculadora;
+
+public class EstudanteTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int num1 = 1;
+        int num2 = 2;
+        calculadora.alteraDoisNumeros(num1, num2);
+        System.out.println("Dentro do CalculadoraTest04");
+        System.out.println("num1 "+num1);
+        System.out.println("num2 "+num2);
+    }
+}
